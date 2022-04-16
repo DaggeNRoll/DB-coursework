372575af4526a623f7fa80c4d4eede693427f07d
Михаил Трифонов <mick3fv@gmaIl.com>
2022-04-16 13:32:34 +0300

    123

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+using System.Text.RegularExpressions;
+
+List<Person> users = new List<Person>
+{
+    new() {Id = Guid.NewGuid().ToString(), Name="Van", Age=69},
+    new(){Id = Guid.NewGuid().ToString(), Name="Billy", Age=38},
+    new() {Id = Guid.NewGuid().ToString(), Name="Ivan", Age=25}
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,30 +14,76 @@ app.Run(async (context) =>
 {
     var response = context.Response;
     var request = context.Request;
-    if (request.Path == "/api/user")
+    var path = request.Path;
+
+    string expressionForGuid = @"^/api/users/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+
+    if(path=="/api/users" && request.Method == "GET")
     {
-        var responseText = "Некорректные данные!";
-
-        if (request.HasJsonContentType())
-        {
-            var person = await request.ReadFromJsonAsync<Person>();
-            if (person != null)
-            {
-                responseText = $"Name: {person.Name} Age: {person.Age}";
-            }
-        }
-
-        await response.WriteAsJsonAsync(new { text = responseText });
+        await GetAllPeople(response);
     }
-
-    else
+    else if(Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
     {
-        response.ContentType = "text/html; charset=utf-8";
-        await response.SendFileAsync("html/json.html");
+        string? id = path.Value?.Split("/")[3];
+        await GetPerson(id, response, request);
     }
+    else if(path=="/api/users" && request.Method == "POST")
+    {
+        await CreatePerson(response, request);
+    }
+    else if(path=="/api/users" && request.Method == "Put")
+    {
 
+    }
 });
 
 app.Run();
 
-public record Person(string Name, int Age);
+async Task GetAllPeople(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(users);
+}
+
+async Task GetPerson(string? id, HttpResponse response, HttpRequest request)
+{
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    if (user != null)
+        await response.WriteAsJsonAsync(user);
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден!" });
+    }
+}
+
+async Task CreatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        var user = await request.ReadFromJsonAsync<Person>();
+        if (user != null)
+        {
+            user.Id = Guid.NewGuid().ToString();
+            users.Add(user);
+            await response.WriteAsJsonAsync(user); 
+        }
+        else
+        {
+            throw new Exception("Некорректные данные!");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные!" });
+    }
+}
+
+
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
+
